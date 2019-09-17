422db914a2a4042c99fc227ba8951f516ac06abd
David Lee <daviddwlee84@gmail.com>
2019-09-17 18:30:00 +0800

    balance data loader and helper

--- data_prepare.py
+++ data_prepare.py
@@ -121,3 +121,85 @@ def tokenize_and_padding(X1, X2, max_len, tokenizer=None, debug=False):
         print('Padded sentences:', padded_token_X1, '\t', padded_token_X2)
 
     return torch.tensor(padded_token_X1, dtype=torch.long), torch.tensor(padded_token_X2, dtype=torch.long)
+
+
+# For balance train
+
+
+def balance_data_loader(random_seed, mode="word", dataset="Ant", test_split=0.3):
+    X1_train, X2_train, Y_train, X1_test, X2_test, Y_test = train_test_data_loader(
+        random_seed, mode, dataset, test_split)
+
+    trainHelper = BalanceDataHelper(X1_train, X2_train, Y_train, random_seed)
+    testHelper = BalanceDataHelper(X1_test, X2_test, Y_test, random_seed)
+
+    return trainHelper, testHelper
+
+
+class BalanceDataHelper:
+    def __init__(self, X1, X2, Y, random_seed):
+        np.random.seed(random_seed)
+        self._seperate_data(X1, X2, Y)
+
+    def _seperate_data(self, X1, X2, Y):
+        positive_index = Y[Y == 1]
+        negative_index = Y[Y == 0]
+
+        self.positive_count = len(positive_index)
+        self.negative_count = len(negative_index)
+
+        self.POS_SENTENCE_PAIR = list(
+            zip(X1[positive_index], X2[positive_index]))
+        self.NEG_SENTENCES = X1[negative_index] + X2[negative_index]
+
+    def _get_negative_samples(self, positive_samples):
+        """ get the same amount of the positive sample by replacing one of the sentence in positive samples """
+        negative_samples = []
+        for pos_sent1, pos_sent2 in positive_samples:
+            # find a unique and non-repeated negative sample
+            while (pos_sent1, pos_sent2) in self.POS_SENTENCE_PAIR or (pos_sent1, pos_sent2) in self.POS_SENTENCE_PAIR:
+                neg_sent = np.random.choice(self.NEG_SENTENCES)
+                if np.random.randint(0, 2):
+                    # replace first sentence
+                    pos_sent1 = neg_sent
+                else:
+                    # replace second sentence
+                    pos_sent2 = neg_sent
+            negative_samples.append((pos_sent1, pos_sent2))
+
+        assert len(positive_samples) == len(negative_samples)
+        return negative_samples
+
+    def batch_iter(self, batch_size, shuffle=True, neg_label=0.0):
+        """ generator of a batch of balance data, neg_label usually be 0 or -1 """
+        positive_data_order = list(range(self.positive_count))
+        if shuffle:
+            np.random.shuffle(positive_data_order)
+
+        assert batch_size % 2 == 0
+        semi_batch_size = batch_size // 2
+
+        for batch_step in range(self.positive_count // semi_batch_size):
+            start_index = batch_step*semi_batch_size
+            end_index = batch_step*semi_batch_size + semi_batch_size
+            if end_index > self.positive_count:
+                end_index = self.positive_count
+            positive_data_index = positive_data_order[start_index:end_index]
+
+            positive_sentence_pair = [self.POS_SENTENCE_PAIR[idx]
+                                      for idx in positive_data_index]
+            negative_sentence_pair = self._get_negative_samples(
+                positive_sentence_pair)
+
+            x_pair = positive_sentence_pair + negative_sentence_pair
+            x1, x2 = zip(*x_pair)  # unzip zipped pair
+            y = [[1.0]] * semi_batch_size + [[neg_label]] * semi_batch_size
+            yield x1, x2, y
+
+
+if __name__ == "__main__":
+    # test
+    trainHelper, _ = balance_data_loader(87)
+    batch_iterator = trainHelper.batch_iter(4)
+    print(next(batch_iterator))
+    print(next(batch_iterator))
