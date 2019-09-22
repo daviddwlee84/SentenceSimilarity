f5d7ceb314cbe5edb5931bfcd889d26076b30fd6
David Lee <daviddwlee84@gmail.com>
2019-09-22 16:12:08 +0800

    use nohup and show script pid when executing it

--- train_all_data_at_once.sh
+++ train_all_data_at_once.sh
@@ -5,13 +5,16 @@ MODEL=${1:-'ERCNN'}
 LOGDIR=log/${MODEL}_All_`date +"%m_%d-%H_%M_%S"`
 
 # Ant
-python3 run.py --dataset Ant --word-segment char --model $MODEL --logdir $LOGDIR &
-python3 run.py --dataset Ant --word-segment word --model $MODEL --logdir $LOGDIR &
+nohup python3 run.py --dataset Ant --word-segment char --model $MODEL --logdir $LOGDIR >& /dev/null &
+nohup python3 run.py --dataset Ant --word-segment word --model $MODEL --logdir $LOGDIR >& /dev/null &
 
 # CCSK
-python3 run.py --dataset CCSK --word-segment char --model $MODEL --logdir $LOGDIR &
-python3 run.py --dataset CCSK --word-segment word --model $MODEL --logdir $LOGDIR &
+nohup python3 run.py --dataset CCSK --word-segment char --model $MODEL --logdir $LOGDIR >& /dev/null &
+nohup python3 run.py --dataset CCSK --word-segment word --model $MODEL --logdir $LOGDIR >& /dev/null &
 
 # PiPiDai
-python3 run.py --dataset PiPiDai --word-segment char --model $MODEL --logdir $LOGDIR &
-python3 run.py --dataset PiPiDai --word-segment word --model $MODEL --logdir $LOGDIR &
+nohup python3 run.py --dataset PiPiDai --word-segment char --model $MODEL --logdir $LOGDIR >& /dev/null &
+nohup python3 run.py --dataset PiPiDai --word-segment word --model $MODEL --logdir $LOGDIR >& /dev/null &
+
+echo "Running scripts"
+ps aux | grep run.py | grep $MODEL
