559bbdc189107851a879530df294b15c5e3dc621
David Lee <daviddwlee84@gmail.com>
2019-09-24 16:38:24 +0800

    change SiameseCNN model structure

--- models/siamese_elements.py
+++ models/siamese_elements.py
@@ -8,28 +8,46 @@ class SingleSiameseCNN(nn.Module):
         super(SingleSiameseCNN, self).__init__()
         self.embedding = nn.Embedding.from_pretrained(
             embedding_matrix, freeze=freeze_embed)
-        # somehow the model in list can't be auto connect `to(device)`
-        self.cnn_filters = [
-            nn.Sequential(
-                nn.Conv1d(max_len, linear_size, kernel_wide),
-                nn.ReLU()
-            ).to(device) for kernel_wide in windows]
+        cnn_feature_num = 256
+        self.cnn_layers = nn.Sequential(
+            nn.Conv1d(max_len, cnn_feature_num,
+                      7, stride=1, padding=3),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(3),
+            nn.Conv1d(cnn_feature_num, cnn_feature_num,
+                      7, stride=1, padding=3),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(3),
+            nn.Conv1d(cnn_feature_num, cnn_feature_num,
+                      7, stride=1, padding=3),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(cnn_feature_num, cnn_feature_num,
+                      7, stride=1, padding=3),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(cnn_feature_num, cnn_feature_num,
+                      7, stride=1, padding=3),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(cnn_feature_num, cnn_feature_num,
+                      7, stride=1, padding=3),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(3)
+        )
+        fcnn_units = 1024
         self.dense = nn.Sequential(
-            nn.Linear(linear_size*len(windows), output_size),
+            nn.Linear(cnn_feature_num*3, fcnn_units),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(fcnn_units, fcnn_units),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(fcnn_units, output_size),
             nn.Sigmoid()
         )
 
     def forward(self, sentence):
         sent_embed = self.embedding(sentence)
-        output_of_cnns = [
-            cnn_filter(sent_embed)
-            for cnn_filter in self.cnn_filters]
-        global_pooled_output = list(map(
-            lambda x: torch.max(x, dim=2)[0], output_of_cnns))
-        # equivalent to
-        # global_pooled_output = [F.max_pool1d(x, x.size(2)).squeeze(2) for x in output_of_cnns]
-        dense_input = torch.cat(global_pooled_output, dim=1)
-        output = self.dense(dense_input)
+        dense_input = self.cnn_layers(sent_embed)
+        output = self.dense(dense_input.view(dense_input.shape[0], -1))
         return output
 
 
@@ -80,4 +98,36 @@ class SingleSiameseLSTM(nn.Module):
         last_hidden_state = lstm_output[:, -1, :]
         # batch_size, output_size
         output = self.dense(last_hidden_state)
+        return output
+
+
+class SingleSiameseTextCNN(nn.Module):
+    """ deprecated """
+
+    def __init__(self, embedding_matrix, max_len, output_size, device, linear_size=128, windows=[3, 4, 5], freeze_embed=False):
+        super(SingleSiameseTextCNN, self).__init__()
+        self.embedding = nn.Embedding.from_pretrained(
+            embedding_matrix, freeze=freeze_embed)
+        # somehow the model in list can't be auto connect `to(device)`
+        self.cnn_filters = [
+            nn.Sequential(
+                nn.Conv1d(max_len, linear_size, kernel_wide),
+                nn.ReLU()
+            ).to(device) for kernel_wide in windows]
+        self.dense = nn.Sequential(
+            nn.Linear(linear_size*len(windows), output_size),
+            nn.Sigmoid()
+        )
+
+    def forward(self, sentence):
+        sent_embed = self.embedding(sentence)
+        output_of_cnns = [
+            cnn_filter(sent_embed)
+            for cnn_filter in self.cnn_filters]
+        global_pooled_output = list(map(
+            lambda x: torch.max(x, dim=2)[0], output_of_cnns))
+        # equivalent to
+        # global_pooled_output = [F.max_pool1d(x, x.size(2)).squeeze(2) for x in output_of_cnns]
+        dense_input = torch.cat(global_pooled_output, dim=1)
+        output = self.dense(dense_input)
         return output
